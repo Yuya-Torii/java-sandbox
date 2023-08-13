f6261ac44987a9e8c6a4f5084286aa873d12623a
YuyaTorii <Yuya_Torii@hulft.com>
2023-08-13 21:57:42 +0900

    Resolved problems, moved/renamed files

---
+++ app/src/main/java/tori/sandbox/designpettern/DesignPettern.java
@@ -0,0 +1,7 @@
+package tori.sandbox.designpettern;
+
+public class DesignPettern {
+    public static void main(String[] args) {
+        
+    }
+}
--- app/src/main/tori/sandbox/designpettern/DesignPettern.java
+++
@@ -1,5 +0,0 @@
-public class DesinPettern {
-    public static void main(String[] args) {
-        
-    }
-}
